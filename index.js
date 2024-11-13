e16b0c7302791be76e63bcce3e0e83a9e6979b6b
tsk-balaji <tskbalaji15@gmail.com>
2024-11-13 21:54:45 +0530

    changes to host in render

--- index.js
+++ index.js
@@ -16,4 +16,6 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/", require("./routes/urlRoutes"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server running on port ${PORT}`)
+);
